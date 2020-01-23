8df5770268c254010e90a5e3c8324495b4c504f7
scuffy99 <sasha.voynevych@gmail.com>
2020-01-23 14:15:12 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+
+  public static void main(String[] args) {
+/*		String name = "Alex";
+    System.out.println(name);
+*/	
+		int x = 50;
+		int y = 8;
+//		double z = 50.0
+		System.out.println(x + y);
+		System.out.println(x - y);
+		System.out.println(x * y);
+		System.out.println((double)x / y); //6.25
+  }
+
+}
