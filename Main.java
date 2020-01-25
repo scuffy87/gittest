65defd9990011f08eface6491d2308830a85b29e
scuffy99 <sasha.voynevych@gmail.com>
2020-01-25 15:15:14 +0200

    New Branch

--- Main.java
+++ Main.java
@@ -6,11 +6,17 @@ class Main {
 */	
 		int x = 50;
 		int y = 8;
+
+		int x2 = 40;
+		int y2 = 6;
 //		double z = 50.0
 		System.out.println(x + y);
 		System.out.println(x - y);
 		System.out.println(x * y);
 		System.out.println((double)x / y); //6.25
+
+		System.out.println(x2 + y2);
+		System.out.println(x2 - y2);
   }
 
 }
